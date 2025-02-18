60aa2c45ae6f1622b6390b1aac32597e6b290287
BL_Coder <126531240+mhynb@users.noreply.github.com>
2025-02-18 20:25:38 +0800

    删除敏感信息

--- src/main/java/com/blcode/reggie/utils/SMSUtils.java
+++ src/main/java/com/blcode/reggie/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tQ7bxjeF2pfuDm4L38A", "pJQtabHdZBXm6QzaMLV9MT4RsQgpLj");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
